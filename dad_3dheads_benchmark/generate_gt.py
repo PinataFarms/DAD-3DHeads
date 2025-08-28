add28f52ad66e8809dfd29411e889c3e8d8531d3
Tetiana Martyniuk <tetyanka.martynyuk@gmail.com>
2025-08-28 23:49:36 +0200

    minor changes in the benchmark generate_gt script

--- dad_3dheads_benchmark/generate_gt.py
+++ dad_3dheads_benchmark/generate_gt.py
@@ -6,18 +6,18 @@ from utils import read_img
 
 def generate_gt(
         base_path: str,
-        output_filename: str = 'data/ground_truth_val.json',
+        subset_name: str ='val'
 ):
-    with open(f'{base_path}/DAD-3DHeadsDataset/val/val.json', "r") as f:
-        val_anno = json.load(f)
+    with open(f'{base_path}/DAD-3DHeadsDataset/{subset_name}/{subset_name}.json', "r") as f:
+        subset_anno = json.load(f)
 
-    val_json = []
-    for el in tqdm(val_anno):
-        annotation_path = f'{base_path}/DAD-3DHeadsDataset/val/annotations/{el["item_id"]}.json'
-        image = read_img(f'{base_path}/DAD-3DHeadsDataset/val/images/{el["item_id"]}.png')
+    subset_json = []
+    for el in tqdm(subset_anno):
+        annotation_path = f'{base_path}/DAD-3DHeadsDataset/{subset_name}/annotations/{el["item_id"]}.json'
+        image = read_img(f'{base_path}/DAD-3DHeadsDataset/{subset_name}/images/{el["item_id"]}.png')
         image_height = image.shape[0]
         anno = json.loads(open(annotation_path).read())
-        val_json.append(
+        subset_json.append(
             {
                 'id': el['item_id'],
                 'bbox': el['bbox'],
@@ -28,8 +28,9 @@ def generate_gt(
             }
         )
 
+    output_filename = f'data/ground_truth_{subset_name}.json'
     with open(output_filename, "w") as out:
-        json.dump(val_json, out)
+        json.dump(subset_json, out)
 
 
 if __name__ == "__main__":
